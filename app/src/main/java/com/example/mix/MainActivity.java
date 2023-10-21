eea1b86921dfcf915d939e6f748a51185036203e
Group7thesis <145889953+Group7thesis@users.noreply.github.com>
2023-10-21 11:49:38 +0800

    Second Commit

--- app/src/main/java/com/example/mix/MainActivity.java
+++ app/src/main/java/com/example/mix/MainActivity.java
@@ -2,13 +2,77 @@ package com.example.mix;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvUsername, tvPassword;
+    EditText etUsername, etPassword;
+    Button btnLogIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvUsername = findViewById(R.id.tvUsername);
+        tvPassword = findViewById(R.id.tvPassword);
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        btnLogIn = findViewById(R.id.btnLogIn);
+
+        btnLogIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new Async().execute();
+            }
+        });
+    }
+
+    public class Async extends AsyncTask<Void, Void, Void> {
+        String username = "";
+        @Override
+        protected Void doInBackground(Void... voids) {
+            try {
+                Class.forName("com.mysql.jdbc.Driver");
+                Connection connection = DriverManager.getConnection(DatabaseConnection.DBCONNECTION, DatabaseConnection.DBUSER, DatabaseConnection.DBPASSWORD);
+                Statement statement = connection.createStatement();
+                ResultSet resultSet = statement.executeQuery("SELECT * FROM user_tbl WHERE user_name='" + etUsername.getText().toString() + "' AND user_password='" + etPassword.getText().toString() + "'");
+                while (resultSet.next()) {
+                    username = resultSet.getString(1);
+                }
+            } catch (ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onPostExecute(Void unused) {
+            super.onPostExecute(unused);
+            if (username.equals(""))
+                Toast.makeText(MainActivity.this, "No User Found", Toast.LENGTH_SHORT).show();
+            else
+                Toast.makeText(MainActivity.this, "Welcome " + username, Toast.LENGTH_SHORT).show();
+        }
     }
 }
